b7931c65a9fdb2e5eccbd7ee12e231dd3cd1356e
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2023-01-10 20:30:05 +0600

    favicon added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <Toaster/>
+
+      <Toaster />
       <App />
   </AuthProvider>
   </React.StrictMode>
