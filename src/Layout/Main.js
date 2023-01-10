ddf0f0d4c76c6eb31362651301087031976cfa22
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2023-01-10 19:26:58 +0600

    drawer added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,14 +1,32 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Drawer from "../Components/Drawer/Drawer";
 import Footer from "../Pages/Shared/Footer";
 import Navbar from "../Pages/Shared/Navbar";
 
 const Main = () => {
   return (
     <div>
-      <Navbar />
-      <Outlet />
-      <Footer />
+    
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+
+        <div className="drawer-content">
+            <Navbar />
+          <div className="w-5/6 mx-auto">
+            <Outlet />
+          </div>
+          <Footer />
+         
+        </div>
+
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+           
+            <Drawer />
+        </div>
+      </div>
+      
     </div>
   );
 };
