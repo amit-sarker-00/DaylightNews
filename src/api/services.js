ee98dec6fe06487405b1685e50e314267bfb78fb
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-01-11 01:00:21 +0600

    error solved in addwriter

--- src/api/services.js
+++ src/api/services.js
@@ -1,12 +1,12 @@
-// post a writer profile
-export const addWriter = async (shopData) => {
+// post a writer
+export const addWriter = async (userData) => {
   const response = await fetch(`http://localhost:5000/writers`, {
     method: "POST",
     headers: {
       "content-type": "application/json",
       authorization: `Bearer ${localStorage.getItem("news-token")}`,
     },
-    body: JSON.stringify(shopData),
+    body: JSON.stringify(userData),
   });
 
   const data = await response.json();
