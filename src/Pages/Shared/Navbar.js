edbb22b770bc76183c92d21549d7d2df9f0e560f
afianasrin1 <afia.nasrin3e@gmail.com>
2023-01-10 21:50:11 +0600

    change

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,110 +1,123 @@
 import React from "react";
+import { AiOutlineBars } from "react-icons/ai";
+import { FaFacebookF, FaGithub, FaGoogle, FaLinkedinIn } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
-
   function formatDate(date) {
     const yyyy = date.getFullYear();
     let dd = date.getDate() + 1;
     if (dd < 10) dd = "0" + dd;
-    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
     ];
-    let strTime =
-      monthNames[date.getMonth()] + "/" + dd + "/" + yyyy;
+    let strTime = monthNames[date.getMonth()] + "/" + dd + "/" + yyyy;
     return strTime;
   }
   const currentDate = formatDate(new Date());
   return (
-    <div>
-      <h1>this is navbar</h1>
-    </div>
+    <main>
+      <section className="bg-[#f0f2f5] pb-5">
+        <div className="w-11/12 mx-auto flex  justify-between py-1">
+          <div>
+            <label
+              htmlFor="my-drawer"
+              className=" cursor-pointer drawer-button"
+            >
+              {" "}
+              <AiOutlineBars />
+            </label>
+          </div>
+
+          <div>
+            <h1>Tempareture </h1>
+          </div>
+        </div>
+
+        <div className="w-11/12 mx-auto items-center flex justify-between">
+          <div>
+            <ul className="flex gap-3">
+              <a className="rounded-full" href="#">
+                <li className="bg-white rounded-full hover:bg-blue-500 transition-all p-2 hover:text-white">
+                  <FaFacebookF className="text-lg" />
+                </li>
+              </a>
+              <a className="rounded-full" href="#">
+                <li className="bg-white rounded-full hover:bg-blue-500 transition-all p-2 hover:text-white">
+                  <FaGoogle className="text-lg" />
+                </li>
+              </a>
+              <a className="rounded-full" href="#">
+                <li className="bg-white rounded-full hover:bg-blue-500 transition-all p-2 hover:text-white">
+                  <FaGithub className="text-lg" />
+                </li>
+              </a>
+              <a className="rounded-full" href="#">
+                <li className="bg-white rounded-full hover:bg-blue-500 transition-all p-2 hover:text-white">
+                  <FaLinkedinIn className="text-lg" />
+                </li>
+              </a>
+            </ul>
+          </div>
+          <div>
+            <h1 className="btn ">DayLight-News</h1>
+          </div>
+          <div>
+            <p className="text-sm font-semibold">{currentDate}</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="my-2">
+        <div className="flex justify-between w-11/12 mx-auto">
+          <div>
+            <ul className="flex gap-3">
+              <li>
+                <Link>Home</Link>
+              </li>
+              <li>
+                <Link>Posts</Link>
+              </li>
+              <li>
+                <Link>MegaMenu</Link>
+              </li>
+              <li>
+                <Link>Pages</Link>
+              </li>
+              <li>
+                <Link>LifeStyle</Link>
+              </li>
+              <li>
+                <Link>Gadgets</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex items-center">
+            <div>
+              <input
+                className="p-1 rounded-2xl pl-3  bg-[#f0f2f5] "
+                type="search"
+                placeholder="Search"
+              />
+            </div>
+            <div>user logo</div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
 export default Navbar;
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// const Navbar = () => {
-//   const menuItems = (
-//     <React.Fragment>
-//       <li>
-//         <Link>Home</Link>
-//       </li>
-//       <li>
-//         <Link>Posts</Link>
-//       </li>
-//       <li>
-//         <Link>MegaMenu</Link>
-//       </li>
-//       <li>
-//         <Link>Pages</Link>
-//       </li>
-//       <li>
-//         <Link>LifeStyle</Link>
-//       </li>
-//       <li>
-//         <Link>Gadgets</Link>
-//       </li>
-//     </React.Fragment>
-//   );
-
-//   return (
-//     <div className="navbar bg-base-100 flex justify-between">
-//       <div className="navbar-start">
-//         <div className="dropdown">
-//           <label tabIndex={0} className="btn btn-ghost lg:hidden">
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               className="h-5 w-5"
-//               fill="none"
-//               viewBox="0 0 24 24"
-//               stroke="currentColor"
-//             >
-//               <path
-//                 strokeLinecap="round"
-//                 strokeLinejoin="round"
-//                 strokeWidth="2"
-//                 d="M4 6h16M4 12h8m-8 6h16"
-//               />
-//             </svg>
-//           </label>
-//           <ul
-//             tabIndex={1}
-//             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-//           >
-//             {menuItems}
-//           </ul>
-//         </div>
-//         <Link to="/" className="btn btn-ghost normal-case text-xl">
-//           Daylight News
-//         </Link>
-//       </div>
-//       <div className="navbar-center hidden lg:flex">
-//         <ul className="menu menu-horizontal p-0">{menuItems}</ul>
-//       </div>
-//       <label
-//         htmlFor="dashboard-drawer"
-//         tabIndex={2}
-//         className="btn btn-ghost lg:hidden"
-//       >
-//         <svg
-//           xmlns="http://www.w3.org/2000/svg"
-//           className="h-5 w-5"
-//           fill="none"
-//           viewBox="0 0 24 24"
-//           stroke="currentColor"
-//         >
-//           <path
-//             strokeLinecap="round"
-//             strokeLinejoin="round"
-//             strokeWidth="2"
-//             d="M4 6h16M4 12h8m-8 6h16"
-//           />
-//         </svg>
-//       </label>
-//     </div>
-//   );
-// };
-// export default Navbar;
