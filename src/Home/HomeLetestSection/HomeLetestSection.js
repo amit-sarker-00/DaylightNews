38ecf442a918c9ab769e2bfb8597ecc24629c2fd
abdulquayumhp <badulquayumhp998@gmailc.com>
2023-01-11 13:32:13 +0600

    first commit

---
+++ src/Home/HomeLetestSection/HomeLetestSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeLetestSection = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <p>bangladesh</p>
+    </div>
+  );
+};
+
+export default HomeLetestSection;
