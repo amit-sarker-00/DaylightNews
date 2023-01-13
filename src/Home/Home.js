ae0ec02333d6de0155583e3ae947490f33c071e3
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2023-01-13 11:09:53 +0600

    hompage updates

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Category from "../Pages/Categoris/Category";
+import HomePageLetestNews from "./HomePageLetestNews/HomePageLetestNews";
 
 const Home = () => {
   return (
     <div className="max-w-[1440px] mx-auto">
       <Category />
-      <h1>this is home and main branch</h1>
+      <HomePageLetestNews/>
     </div>
   );
 };
