180020e2adf884f0e074979e0ff5b32f3cb01e81
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-01-24 21:06:04 +0600

    DivisionHorizontalData

---
+++ src/Home/DistricNews/DivisionHorizontalData .js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const DivisionHorizontalData = ({ data }) => {
+  const { description, author, category, picture, title } = data;
+  return (
+    <Link className="w-full ">
+      <img className="md:full w-full " src={picture} alt="" />
+      <h1 className="bg-slate-800 rounded-b-lg text-white p-2 text-sm">
+        {title.slice(0, 30) + "..."}
+      </h1>
+    </Link>
+  );
+};
+
+export default DivisionHorizontalData;
