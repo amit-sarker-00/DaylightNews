4c40741c4e6ec8c3747a417822f50e3d6bf5fec9
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2023-02-10 09:32:03 +0600

    merged data

--- src/Components/SpacialNews/SpacialNews.js
+++ src/Components/SpacialNews/SpacialNews.js
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from 'react';
 import "../SpacialNews/SpacialNews.css"
 import Marquee from "react-fast-marquee";
 
-const SpacialNews = () => {
+const SpacialNews = () =>
+{
 
 
-    const [bannerData, setBannerData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
+    const [ bannerData, setBannerData ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+    useEffect(() =>
+    {
         fetch(`sylet.json`)
             .then((res) => res.json())
-            .then((data) => {
+            .then((data) =>
+            {
                 setBannerData(data);
                 setLoading(false);
             });
@@ -25,8 +28,8 @@ const SpacialNews = () => {
                 </div>
 
                 <Marquee pauseOnHover >
-                    {bannerData?.map((item) => (
-                        <> {item?.title} || </>
+                    {bannerData?.map((item, i) => (
+                        <div key={i}> {item?.title} || </div>
                     ))}
                 </Marquee>
 
